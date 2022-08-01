cab43ac932fb792af6287a5f19d89cac934df709
rob-digital <robert.roksela@gmail.com>
2022-08-01 15:57:26 +0100

    BaseEntity added.

--- pet_clinic-data/src/main/java/guru/springframework/javaspring_petclinic/model/Person.java
+++ pet_clinic-data/src/main/java/guru/springframework/javaspring_petclinic/model/Person.java
@@ -1,6 +1,6 @@
 package guru.springframework.javaspring_petclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
 
     private String firstName;
     private String lastName;
--- pet_clinic-data/src/main/java/guru/springframework/javaspring_petclinic/model/PetType.java
+++ pet_clinic-data/src/main/java/guru/springframework/javaspring_petclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package guru.springframework.javaspring_petclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
 
     private String name;
 
