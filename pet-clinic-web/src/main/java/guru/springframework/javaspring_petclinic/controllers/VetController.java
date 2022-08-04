8424d9bf5eb91c5657e108eea929dfb2b0938f33
rob-digital <robert.roksela@gmail.com>
2022-08-04 10:26:55 +0100

    Adding Vets page and controller.

---
+++ pet-clinic-web/src/main/java/guru/springframework/javaspring_petclinic/controllers/VetController.java
@@ -0,0 +1,13 @@
+package guru.springframework.javaspring_petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping("/vets")
+    public String listVets() {
+        return "vets/index";
+    }
+}
