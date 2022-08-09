9253f5137f1c2dad02da2db9bb08c0f4333eac84
rob-digital <robert.roksela@gmail.com>
2022-08-09 13:11:25 +0100

    Static resources (fonts & images) added.

--- pet-clinic-web/src/main/java/guru/springframework/javaspring_petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/javaspring_petclinic/bootstrap/DataLoader.java
@@ -44,14 +44,12 @@ public class DataLoader implements CommandLineRunner {
 //            Speciality savedDentistry = specialtyService.save(dentistry);
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
 
@@ -61,14 +59,12 @@ public class DataLoader implements CommandLineRunner {
 
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
 
